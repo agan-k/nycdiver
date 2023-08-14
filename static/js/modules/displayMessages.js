87678b3dc23d33b92e9188c81b99c0c6e802f3b2
agan-k <koranagan@gmail.com>
2023-08-14 18:48:10 -0400

    refactor display messages

--- static/js/modules/displayMessages.js
+++ static/js/modules/displayMessages.js
@@ -2,29 +2,28 @@ import { MESSAGES } from "./constants";
 
 export default function displayMessages() {
   if (MESSAGES.length === 0) return;
-  if (MESSAGES.length > 1) {
-    flashTempMessages(MESSAGES);
-  }
-  MESSAGES[0].classList.add('js-show');
-}
 
-function flashTempMessages(messages) {
-  messages.forEach((m, i) => {
+  MESSAGES.forEach((m, index) => {
     const isTempMessage = Boolean(
-      m.classList.contains('message--info') || m.classList.contains('message--success')
-    );
+    m.classList.contains('message--info') || 
+    m.classList.contains('message--success'));
+    const hasMultipleMessages = Boolean(MESSAGES.length > 1);
 
     m.classList.add('js-show');
 
-    if (isTempMessage) {
-      if (i !== messages.length - 1) {
+    if (isTempMessage && hasMultipleMessages) {
+      if (index !== MESSAGES.length - 1) {
         m.classList.add('js-merge');
       }
-
       setTimeout(() => {
         m.classList.remove('js-show');
         m.classList.remove('js-merge');
       }, 4000);
     };
+    if (isTempMessage) {
+      setTimeout(() => {
+        m.classList.remove('js-show');
+      }, 4000);
+    }
   });
 }
