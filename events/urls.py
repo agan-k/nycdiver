05e6c238c404e22d1e3c5e7a245c91877a9c4fb2
agan-k <koranagan@gmail.com>
2023-08-18 11:09:37 -0400

    remove comments

--- events/urls.py
+++ events/urls.py
@@ -10,7 +10,5 @@ urlpatterns = [
   path('search_events', views.search_events_view, name='search-events'),
   path('event_add/', views.add_event, name='event-add'),
   path('event_update/<event_id>', views.update_event, name='event-update'),
-  # path('event_delete/<event_id>', views.delete_event, name='event-delete'),
   path('toggle_user_event_delete/<event_id>', views.toggle_user_event_delete, name='toggle-user-event-delete'),
-  # path('delete_expired_events/', views.delete_expired_events),
 ]
