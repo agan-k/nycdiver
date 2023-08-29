d32e8bb1d756d8f0e7506f3d3b9fdf2a9b69c2dc
agan-k <koranagan@gmail.com>
2023-08-29 12:11:38 -0400

    override whitenoise to test static files

--- nycdiver/settings.py
+++ nycdiver/settings.py
@@ -144,4 +144,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Simplified static file serving.
 # https://pypi.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
